d9ed72ed81d2e27223a09d86c3f1ba740f710286
MaHryCT3 <mahryct123@gmail.com>
2023-01-30 11:30:12 +0500

    🔨fix(storage): igonre ingerity error in report

--- app/services/storage/report_controller.py
+++ app/services/storage/report_controller.py
@@ -1,5 +1,8 @@
 from typing import TYPE_CHECKING
 
+from loguru import logger
+from sqlalchemy.exc import IntegrityError
+
 from app.services.storage import crud
 from app.services.storage.schemas.reports import ReportsCreate
 from app.services.storage.session import get_session
@@ -10,10 +13,14 @@ if TYPE_CHECKING:
 
 class ReportController:
     async def add_report(self, report_message: 'ReportMessage') -> None:
+        logger.debug(f'add report - {report_message}')
         obj_in_report_message = ReportsCreate(
             author_nickname=report_message.author_nickname,
             report_steamid=report_message.report_steamid,
             server_number=report_message.server_number,
         )
         async with get_session() as session:
-            await crud.reports.create(session, obj_in=obj_in_report_message)
+            try:
+                await crud.reports.create(session, obj_in=obj_in_report_message)
+            except IntegrityError:
+                logger.debug('Report already exist, skip..')
