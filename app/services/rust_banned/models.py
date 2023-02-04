6e66a627e8280b673714aaac1fe5ca1dcf46e2e1
MaHryCT3 <mahryct123@gmail.com>
2023-02-04 10:47:38 +0500

    🔨fix(rust banned): fix validation eac bac count

--- app/services/rust_banned/models.py
+++ app/services/rust_banned/models.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 
 
 class RustBannedResponse(BaseModel):
@@ -6,3 +6,9 @@ class RustBannedResponse(BaseModel):
     twitter_url: str | None = Field(None, alias='url')
     days_ago: int | None = None
     is_banned: bool = Field(..., alias='eac_ban_count')
+
+    @validator('is_banned', pre=True)
+    def validate_eac_ban_count(cls, value: int) -> int:
+        if int(value) > 1:
+            return 1
+        return int(value)
