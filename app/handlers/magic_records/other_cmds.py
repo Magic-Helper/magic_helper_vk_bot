272a321d1c61cc373c42e7f3f20ed9dfbbcd8ec2
MaHryCT3 <mahryct123@gmail.com>
2023-06-16 22:05:47 +0500

    dont parse links = True

--- app/handlers/magic_records/other_cmds.py
+++ app/handlers/magic_records/other_cmds.py
@@ -9,4 +9,4 @@ BASE_STEAM_LINK = 'https://steamcommunity.com/profiles/'
 
 @other_cmds_labeler.message(rules.VBMLRule(patterns.link_cmd))
 async def get_link_by_steamid(message: Message, steamid: str) -> None:
-    await message.answer(BASE_STEAM_LINK + steamid)
+    await message.answer(BASE_STEAM_LINK + steamid, dont_parse_links=True)
