aee3307ca58501a2bf4c8b14f14cc4e39b4f31da
MaHryCT3 <mahryct123@gmail.com>
2023-12-10 15:16:52 +0500

    fix start messsage pattern

--- app/core/patterns.py
+++ app/core/patterns.py
@@ -1,7 +1,7 @@
 from vbml import Pattern
 
 check_start_msg = Pattern(
-    '[id<moder_id:int>|<any>] твоя команда выполнена.\nОтвет: <nickname> вызван на проверку. Напишите /cc2 <server_number:int> <steamid> для отмены проверки.\n<trash>'
+    '[id<moder_id:int>|<any>] твоя команда выполнена.\nОтвет: <nickname> вызван на проверку. Напишите /cc2 <server_number:int> <steamid> для отмены проверки.<trash>'
 )
 
 check_end_msg = Pattern('[id<moder_id:int>|<any>] твоя команда выполнена.\nОтвет: <nickname> больше не проверяется.')
